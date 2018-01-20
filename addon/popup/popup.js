155f52972a5a21c44a61a8ff8da03bc00fcf4d99
chromia <chromia@outlook.jp>
2018-01-21 07:30:14 +0900

    remove unused code came from examples

--- addon/popup/popup.js
+++ addon/popup/popup.js
@@ -20,9 +20,3 @@ browser.runtime.sendMessage("getstatus", function (response){
     }
     elem_target.textContent = "Target: " + targetname;
 });
-
-function reportExecuteScriptError(error) {
-  document.querySelector("#popup-content").classList.add("hidden");
-  document.querySelector("#error-content").classList.remove("hidden");
-  console.error(`Failed to execute beastify content script: ${error.message}`);
-}
