21415fbb51cb603542be5625ecd48bacef93cac5
chromia <chromia@outlook.jp>
2018-01-28 12:12:38 +0900

    update publishing directory structure

--- addon/publish.sh
+++ addon/publish.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/sh
 
+#For Firefox
 rm -rf pub_firefox
-#rm -rf pub_chrome
-#rm -rf pub_edge
-
 cp -r base pub_firefox
-#cp -r base pub_chrome
-#cp -r base pub_edge
+cp manifests/firefox/manifest.json pub_firefox/manifest.json
 
-cp manifests/manifest.firefox pub_firefox/manifest.json
-#cp manifests/manifest.chrome pub_chrome/manifest.json
-#cp manifests/manifest.edge pub_edge/manifest.json
+#For Edge
+#Do nothing here. use /native/windows/edge/memoryobserverForEdge.sln
+#It is all-in-one solution for publishing
+
+#For Chrome(not supported yet)
+#rm -rf pub_chrome
+#cp -r base pub_chrome
+#cp manifests/chrome/manifest.json pub_chrome/manifest.json
