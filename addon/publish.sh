fa7b57cb0fbcf76e29ecf7e23fece1fc0978e69b
chromia <chromia@outlook.jp>
2018-02-01 03:02:07 +0900

    update publishing script for chrome

--- addon/publish.sh
+++ addon/publish.sh
@@ -10,6 +10,6 @@ cp manifests/firefox/manifest.json pub_firefox/manifest.json
 #It is all-in-one solution for publishing
 
 #For Chrome(not supported yet)
-#rm -rf pub_chrome
-#cp -r base pub_chrome
-#cp manifests/chrome/manifest.json pub_chrome/manifest.json
+rm -rf pub_chrome
+cp -r base pub_chrome
+cp manifests/chrome/manifest.json pub_chrome/manifest.json
